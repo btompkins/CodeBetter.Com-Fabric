6423043bd5abd385f7d7b60352e0e77234c609da
btompkins <brendan.tompkins@gmail.com>
2011-01-17 16:10:22 -0500

    Added a whole bunch of scripting stuff to setup a bare-bones LAMP server and website under Ubuntu.

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,110 @@
 from fabric.api import *
+from fabric.contrib.files import *
 
 env.hosts = ['184.106.93.74']
-env.password = '[PASSWORD]'
+env.user = 'brendan'
 
-
-def new_user(username, password):
+def new_user(admin_username, admin_password):   
     env.user = 'root'
-    sudo('useradd %s -p %s' % (username, password), pty=True)
+    env.password = 'test.codebetter.comtx65bUXO4'
+    
+    # Create the admin group and add it to the sudoers file
+    admin_group = 'admin'
+    runcmd('addgroup {group}'.format(group=admin_group))
+    runcmd('echo "%{group} ALL=(ALL) ALL" >> /etc/sudoers'.format(
+        group=admin_group))
+    
+    # Create the new admin user (default group=username); add to admin group
+    runcmd('adduser {username} --disabled-password --gecos ""'.format(
+        username=admin_username))
+    runcmd('adduser {username} {group}'.format(
+        username=admin_username,
+        group=admin_group))
+    
+    # Set the password for the new admin user
+    runcmd('echo "{username}:{password}" | chpasswd'.format(
+        username=admin_username,
+        password=admin_password))
+    
+def upgrade_hosts():
+    runcmd('apt-get -y update && apt-get -y dist-upgrade ')
 
-def upgrade_hosts(username):
-    env.user=username
-    sudo('apt-get -y update && apt-get -y dist-upgrade ', pty=True)
+def install_apache():
+    runcmd('apt-get -y install apache2 php5 libapache2-mod-php5')
+    runcmd('a2enmod rewrite')
+    runcmd('a2enmod deflate')
+    runcmd('a2enmod expires')
+    runcmd('a2enmod headers')
+    runcmd('sh -c "echo \'<?php phpinfo( ); ?>\'  > /var/www/info.php"')
+    append(['',
+            '# Customizations', 'Header unset ETag',
+            'FileETag None',
+            'ExpiresActive On',
+            'ExpiresDefault "access plus 7 days"'], '/etc/apache2/apache2.conf', use_sudo=True)
+    runcmd('/etc/init.d/apache2 restart')    
+          
+def install_mysql(mysql_root_password):
+    runcmd('echo "mysql-server mysql-server/root_password select {password}" |' 
+           'debconf-set-selections'.format(
+        password=mysql_root_password))
+    runcmd('echo "mysql-server mysql-server/root_password_again select ' 
+           '{password}" | debconf-set-selections'.format(
+        password=mysql_root_password))
+    runcmd('apt-get -y install mysql-server mysql-client php5-mysql')
+
+def install_phpmyadmin():
+    runcmd('DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin')
+    runcmd('ln -sv /etc/phpmyadmin/apache.conf '
+           '/etc/apache2/conf.d/phpmyadmin.conf')
+    runcmd('/etc/init.d/apache2 restart')
+    # Now you can point your browser to: http://domain/phpmyadmin    
+
+def install_mail():
+    runcmd('DEBIAN_FRONTEND=noninteractive apt-get -y install postfix')
+
+def install_ftp():
+    runcmd('apt-get -y install vsftpd')
+    uncomment('/etc/vsftpd.conf', 'write_enable=YES', use_sudo=True)
+    uncomment('/etc/vsftpd.conf', 'local_umask=022', use_sudo=True)   
+    uncomment('/etc/vsftpd.conf', 'chroot_local_user=YES', use_sudo=True)
+    runcmd('sudo /etc/init.d/vsftpd start')
+
+def setup_website(domain_name):
+    runcmd('mkdir /var/www/{domain}'.format(domain=domain_name))
+    runcmd('touch /etc/apache2/sites-enabled/{domain}'.format(
+        domain=domain_name))
+    append(['NameVirtualHost *:80',
+            '',
+            '<VirtualHost *:80>',
+            '   ServerName www.{domain}'.format(domain=domain_name),
+            '   ServerAlias {domain} *.{domain}'.format(domain=domain_name),
+            '   DocumentRoot /var/www/{domain}'.format(domain=domain_name),
+            '</VirtualHost>'],'/etc/apache2/sites-enabled/{domain}'.format(
+                domain=domain_name), use_sudo=True)
+    # Note that the following will only work once!
+    append(['<IfModule mod_rewrite.c>',
+        '   RewriteLog "/var/log/apache2/rewrite.log"',
+        '   RewriteLogLevel 1',
+        '   RewriteMap rewritemap txt:/var/www/{domain}/permalinkmap.txt'.format(domain=domain_name),
+        '   LimitInternalRecursion 5',
+        '</IfModule>'], '/etc/apache2/apache2.conf', use_sudo=True)
+                      
+def install_git():
+    runcmd('apt-get -y install git-core')
+
+    
+# Helpers    
+def runcmd(arg):
+    if env.user != "root":
+        sudo("%s" % arg, pty=True)
+    else:
+        run("%s" % arg, pty=True)
+
+
+"""
+Thanks to
+
+http://www.howtoforge.com/ubuntu_debian_lamp_server
+http://serverfault.com/questions/122954/secure-method-of-changing-a-users-password-via-python-script-non-interactively
+
+"""
